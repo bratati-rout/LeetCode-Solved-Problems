eff9ee7ae901930b268b9818f168914167be744d
Bratati Rout <bratatirout29@gmail.com>
2022-09-10 22:45:40 +0530

    Time: 5 ms (15.45%), Space: 44.5 MB (77.25%) - LeetHub

--- 152-maximum-product-subarray/152-maximum-product-subarray.java
+++ 152-maximum-product-subarray/152-maximum-product-subarray.java
@@ -1,26 +1,14 @@
 class Solution {
     public int maxProduct(int[] nums) {
-        int max_ending_here = nums[0];
-  
-    // min negative product ending
-    // at the current position
-    int min_ending_here = nums[0];
-  
-    // Initialize overall max product
-    int max_so_far = nums[0];
-  
-    // /* Traverse through the array.
-    // the maximum product subarray ending at an index
-    // will be the maximum of the element itself,
-    // the product of element and max product ending previously
-    // and the min product ending previously. */
-    for(int i=1;i<nums.length;i++){
-      int temp = Math.max(Math.max(nums[i], nums[i] * max_ending_here), nums[i] * min_ending_here);
-      min_ending_here = Math.min(Math.min(nums[i], nums[i] * max_ending_here), nums[i] * min_ending_here);
-      max_ending_here = temp;
-      max_so_far = Math.max(max_so_far, max_ending_here);
-    }
-  
-    return max_so_far;
+       int min=nums[0],max=nums[0],finalMax=nums[0];
+        for(int i=1;i<nums.length;i++)
+        {
+            int tempMax=nums[i]*max;
+            int tempMin=nums[i]*min;
+            max=Math.max(Math.max(tempMax,tempMin),nums[i]);
+            min=Math.min(Math.min(tempMax,tempMin),nums[i]);
+            finalMax=Math.max(finalMax,max);
+        }
+        return finalMax;
     }
 }
