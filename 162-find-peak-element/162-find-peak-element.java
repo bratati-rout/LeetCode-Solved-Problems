cfc3e2308b4d27fe627c0f4e31675f1d83ad03b5
Bratati Rout <bratatirout29@gmail.com>
2022-09-11 23:27:46 +0530

    Time: 1 ms (12.52%), Space: 42.1 MB (85.74%) - LeetHub

---
+++ 162-find-peak-element/162-find-peak-element.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int findPeakElement(int[] nums) {
+        
+        int left=0;
+        int right=nums.length-1;
+        int peak=0;
+        while(left<right)
+        {
+            int mid=left+(right-left)/2;
+            
+            if(nums[mid]<nums[mid+1])
+                left=mid+1;
+            else
+                right=mid;
+        }
+        return left;
+    }
+}
