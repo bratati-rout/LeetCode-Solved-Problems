586fe24ed33db066d3b5543e6f49cbe33cc975ff
Bratati Rout <bratatirout29@gmail.com>
2022-09-17 21:40:32 +0530

    Time: 5 ms (25.15%), Space: 44.9 MB (62.40%) - LeetHub

---
+++ 142-linked-list-cycle-ii/142-linked-list-cycle-ii.java
@@ -0,0 +1,37 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        
+        ListNode slow=head;
+        ListNode fast=head;
+        ListNode entry=head;
+        if(head==null || head.next==null)
+            return null;
+        while(fast!=null && fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
+            if(fast==slow)
+            {
+                while(slow!=entry)
+                {
+                    entry=entry.next;
+                    slow=slow.next;
+                }
+                 return entry;   
+            }
+        }
+       
+        return null;
+    }
+}
