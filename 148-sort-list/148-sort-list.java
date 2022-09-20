318fa002b8c17caf809793bbaa9e0b530bfd622e
Bratati Rout <bratatirout29@gmail.com>
2022-09-20 20:50:06 +0530

    Time: 37 ms (13.40%), Space: 86 MB (5.03%) - LeetHub

--- 148-sort-list/148-sort-list.java
+++ 148-sort-list/148-sort-list.java
@@ -12,51 +12,44 @@ class Solution {
     public ListNode sortList(ListNode head) {
       
     
-        if(head==null || head.next == null){
+        if(head==null || head.next==null)
             return head;
-        }
-        ListNode middle = findMiddle(head);
-        ListNode nextOfMiddle = middle.next;
-        middle.next=null;
-        ListNode left = sortList(head);
-        ListNode right = sortList(nextOfMiddle);
-        ListNode res = merge(left , right);
-        return res;
+        ListNode left=head;
+        ListNode right=getMid(head);
+        ListNode temp=right.next;
+        right.next=null;
+        right=temp;
+        left=sortList(left);
+        right=sortList(right);
+        return merge(left,right);
     }
-    public ListNode findMiddle(ListNode head)
+    public ListNode getMid(ListNode head)
     {
-      ListNode slow=head;
-        ListNode fast =head;
-        while(fast.next!=null && fast.next.next!=null){
-            slow = slow.next;
-            fast = fast.next.next;
+        ListNode slow=head;
+        ListNode fast=head.next;
+        while(fast!=null && fast.next!=null)
+        {
+            slow=slow.next;
+            fast=fast.next.next;
         }
         return slow;
     }
-    public ListNode merge(ListNode head1, ListNode head2){
-        ListNode temp = new ListNode(0);
-        ListNode sorted = temp;
-        while(head1!=null && head2!=null){
-            if(head1.val<=head2.val){
-                sorted.next = head1;
-                head1 = head1.next;
-            }
-            else{
-                sorted.next = head2;
-                head2 = head2.next;
-            }
-            sorted = sorted.next;
+    public ListNode merge(ListNode left, ListNode right)
+    {
+        if(left == null)
+            return right;
+        if(right == null)
+            return left;
+        if(left.val < right.val)
+        {
+            left.next = merge(left.next, right);
+            return left;
         }
-        while(head1!=null){
-            sorted.next = head1;
-            sorted = sorted.next;
-            head1 = head1.next;
+        else
+        {
+            right.next = merge(left, right.next);
+            return right;
         }
-        while(head2!=null){
-            sorted.next = head2;
-            sorted = sorted.next;
-            head2 = head2.next;
-        }
-        return temp.next;
+        
     }
 }
